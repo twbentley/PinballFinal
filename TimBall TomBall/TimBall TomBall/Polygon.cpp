7425ca7f3e7696ff253e2f20c2625233f5fd8f92
unknown <twb1193@C2435-8.main.ad.rit.edu>
2013-05-06 12:20:24 -0400

    Committing changes made to Polygon.cpp that worked with the other files I last committed.
    Signed-off-by: unknown <twb1193@C2435-8.main.ad.rit.edu>

--- TimBall TomBall/TimBall TomBall/Polygon.cpp
+++ TimBall TomBall/TimBall TomBall/Polygon.cpp
@@ -2,7 +2,7 @@
 
 Polygon::Polygon()
 {
-	num_sides = 3;
+	num_sides = 4;
 	radius = 0.5f;
 	//points = new Vector2[num_sides];
 	points = new Vector4[num_sides];
@@ -12,15 +12,18 @@ Polygon::Polygon()
 	//{
  //       // Set the angle and use that to get x and y points (first point will fall on 2PI/(0,1))
  //       theta += static_cast<GLfloat>( (2 * PI / 1) / num_sides);
- //       x = static_cast<GLfloat>( radius * cos(theta + PI / 2));
- //       y = static_cast<GLfloat>( radius * sin(theta + PI / 2));
+ //       x = static_cast<GLfloat>( 100 * radius * cos(theta + PI / 2));
+ //       y = static_cast<GLfloat>( 100 * radius * sin(theta + PI / 2));
  //       
 	//	points[i] = Vector4(x, y, 0, 0);
  //   }
 
-	points[0] = Vector4(-200.0f, -200.0f, 0, 1);
-	points[1] = Vector4(-200.0f, 200.0f, 0, 1);
-	points[2] = Vector4(200.0f, 200.0f, 0, 1);
+	points[0] = Vector4(-100.0f, -100.0f, 0, 1);
+	points[1] = Vector4(-100.0f, 100.0f, 0, 1);
+	points[2] = Vector4(100.0f, 100.0f, 0, 1);
+	points[3] = Vector4(100.0f, -100.0f, 0, 1);
+	//points[4] = Vector4(125.0f, 200.0f, 0, 1);
+	//points[5] = Vector4(0.0f, 100.0f, 0, 1);
 
 	glGenBuffers( 1, &buffer );
 	glBindBuffer( GL_ARRAY_BUFFER, buffer);
@@ -34,7 +37,7 @@ Polygon::~Polygon(void)
 // Draw a polygon using an array of points
 void Polygon::Draw(ColorShader* shader, Matrix4* worldMatrix, Matrix4* viewMatrix, Matrix4* projectionMatrix)
 {
-	Vector4 temp[3];
+	Vector4 temp[4];
 	temp[0] = Vector4( *worldMatrix * points[0] );
 	temp[0] = Vector4(*viewMatrix * temp[0] );
 	temp[0] = Vector4(*projectionMatrix * temp[0] );
@@ -44,6 +47,9 @@ void Polygon::Draw(ColorShader* shader, Matrix4* worldMatrix, Matrix4* viewMatri
 	temp[2] = Vector4( *worldMatrix * points[2] );
 	temp[2] = Vector4(*viewMatrix * temp[2] );
 	temp[2] = Vector4(*projectionMatrix * temp[2] );
+	temp[3] = Vector4( *worldMatrix * points[3] );
+	temp[3] = Vector4(*viewMatrix * temp[3] );
+	temp[3] = Vector4(*projectionMatrix * temp[3] );
 
 	glBindBuffer( GL_ARRAY_BUFFER, buffer);
 	shader->Render(worldMatrix, viewMatrix, projectionMatrix, num_sides);
