54bc35a8ff33a7bbb9e039d8ab2d12f1f78e3d8e
unknown <tmr9209@GDD-42.main.ad.rit.edu>
2013-05-11 16:33:30 -0400

    Added color changes to shaders.

--- TimBall TomBall/TimBall TomBall/vshader.glsl
+++ TimBall TomBall/TimBall TomBall/vshader.glsl
@@ -1,14 +1,18 @@
 #version 150
 
 in vec4 vPosition;
+out vec4 color;
 
 uniform mat4 worldMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 projectionMatrix;
+uniform vec4 vColor;
 
 void main()
 {
 	gl_Position = worldMatrix * vPosition;
 	gl_Position = viewMatrix * gl_Position;
 	gl_Position = projectionMatrix * gl_Position;
+
+	color = vColor;
 }
